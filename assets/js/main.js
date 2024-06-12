4139818cc85dedd6e147e79a6f7bbbaf3540e7af
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-06-12 17:20:11 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,40 @@ elements.forEach((element, index) => {
     element.classList.add('loaded');
   }, index * 70); // Add a delay of 100ms for each element
 });
+
+
+
+//---
+
+
+
+//typing-effect.js
+
+function typingEffect(text, element) {
+  let currentChar = 0;
+  const interval = setInterval(() => {
+    if (currentChar < text.length) {
+      element.textContent += text[currentChar];
+      currentChar++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 100); // adjust this value to change the typing speed
+}
+
+// Select all elements with the class "typing-effect"
+const elements = document.querySelectorAll('.typing-effect');
+
+// Apply the typing effect to each element
+elements.forEach((element) => {
+  const text = element.getAttribute('data-text');
+  typingEffect(text, element);
+});
+
+
+
+//---
+
+
+
+//null.js
